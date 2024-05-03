0a7d93f509e766e7512949c4c7ad84017f7a56ee
Oscar Merida <oscar@oscarm.org>
2024-05-03 00:46:28 -0400

    Fixing notation for response properties

--- src/Response/My/Agent.php
+++ src/Response/My/Agent.php
@@ -7,15 +7,16 @@ use Phparch\SpaceTraders\Response\Base;
 class Agent extends Base
 {
     public function __construct(
-        /** @param non-empty-string */
+        /** @var non-empty-string */
         public readonly string $accountId,
-        /** @param non-empty-string */
+        /** @var non-empty-string */
         public readonly string $symbol,
-        /** @param non-empty-string */
+        /** @var non-empty-string */
         public readonly string $headquarters,
         public readonly int $credits,
-        /** @param non-empty-string */
+        /** @var non-empty-string */
         public readonly string $startingFaction,
+        /** @var non-negative-int */
         public readonly int $shipCount
     ) {}
 }
