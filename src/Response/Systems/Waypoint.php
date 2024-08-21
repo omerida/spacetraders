9509700f825a04a983454754555894fb89050e86
Oscar Merida <oscar@oscarm.org>
2024-08-20 23:18:30 -0400

    Add a command to get a list of waypoints by type in a system.

---
+++ src/Response/Systems/Waypoint.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Phparch\SpaceTraders\Response\Systems;
+
+use Phparch\SpaceTraders\Response\Base;
+use Phparch\SpaceTraders\Value\SystemSymbol;
+use Phparch\SpaceTraders\Value\WaypointSymbol;
+
+class Waypoint extends Base
+{
+    public function __construct(
+        public SystemSymbol $systemSymbol,
+        public WaypointSymbol $symbol,
+        /** @var non-empty-string */
+        public readonly string $type,
+        public readonly int $x,
+        public readonly int $y,
+        /** @var list<\Phparch\SpaceTraders\Value\Orbital> */
+        public readonly array $orbitals,
+        /** @var list<\Phparch\SpaceTraders\Value\SystemTrait> */
+        public readonly array $traits,
+        /** @var list<\Phparch\SpaceTraders\Value\SystemTrait> */
+        public readonly array $modifiers,
+        /** @var array{submittedBy: string, submittedOn: string } */
+        public readonly array $chart,
+        /** @var array{symbol: string } */
+        public readonly array $faction,
+        public readonly bool $isUnderConstruction
+    ) {}
+}
--- src/Response/Systems/Waypoints.php
+++ src/Response/Systems/Waypoints.php
@@ -3,28 +3,11 @@
 namespace Phparch\SpaceTraders\Response\Systems;
 
 use Phparch\SpaceTraders\Response\Base;
-use Phparch\SpaceTraders\Value\SystemSymbol;
-use Phparch\SpaceTraders\Value\WaypointSymbol;
 
 class Waypoints extends Base
 {
     public function __construct(
-        public SystemSymbol $systemSymbol,
-        public WaypointSymbol $symbol,
-        /** @var non-empty-string */
-        public readonly string $type,
-        public readonly int $x,
-        public readonly int $y,
-        /** @var list<\Phparch\SpaceTraders\Value\Orbital> */
-        public readonly array $orbitals,
-        /** @var list<\Phparch\SpaceTraders\Value\SystemTrait> */
-        public readonly array $traits,
-        /** @var list<\Phparch\SpaceTraders\Value\SystemTrait> */
-        public readonly array $modifiers,
-        /** @var array{submittedBy: string, submittedOn: string } */
-        public readonly array $chart,
-        /** @var array{symbol: string } */
-        public readonly array $faction,
-        public readonly bool $isUnderConstruction
+        /** @var \Phparch\SpaceTraders\Response\Systems\Waypoint[] */
+        public array $ships,
     ) {}
 }
