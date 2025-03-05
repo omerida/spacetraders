8ae96574d72cca314e3398d741544f904da1674a
Oscar Merida <oscar@oscarm.org>
2025-03-05 00:00:38 -0500

    Addings PSR-7 routing

--- web/index.php
+++ web/index.php
@@ -1,3 +1,24 @@
-<?php
+<?php declare(strict_types=1);
 
-echo "Hello World";
+use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+// include the Composer autoloader
+require '../vendor/autoload.php';
+
+$request = \GuzzleHttp\Psr7\ServerRequest::fromGlobals();
+
+$router = new League\Route\Router();
+
+// map a route
+$router->map('GET', '/', function (ServerRequestInterface $request): ResponseInterface {
+    $response = new \GuzzleHttp\Psr7\Response();
+    $response->getBody()->write('<h1>Hello, World!</h1>');
+    return $response;
+});
+
+$response = $router->dispatch($request);
+
+// send the response to the browser
+(new SapiEmitter)->emit($response);
