10efed5d9e8cd87a6a7825a34aee08439c444673
Oscar Merida <oscar@oscarm.org>
2025-06-08 23:41:26 -0400

    Control cache reading with env var.

--- src/RoutesMapper.php
+++ src/RoutesMapper.php
@@ -26,55 +26,16 @@ class RoutesMapper
          */
         private readonly array $controllerDirs,
         private Container $container,
+        private bool $useAPCu,
     ) {
         $this->ref = new BetterReflection();
     }
 
     public function registerAll(Router $router): Router
     {
-        $controllers = $this->findControllers();
-
         // 1. Build an array of the discovered routes that we
         //    can cache so we don't do this on every request.
-        $discovered = [];
-        foreach ($controllers as $classInfo) {
-            $methods = $classInfo->getImmediateMethods(
-                filter: \ReflectionMethod::IS_PUBLIC
-            );
-            foreach ($methods as $method) {
-                if ($method->getName() === '__construct') {
-                    continue;
-                }
-
-                if ($attrs = $method->getAttributesByName(Route::class)) {
-                    foreach ($attrs as $attr) {
-                        /**
-                         * @var array{
-                         *     name: string,
-                         *     path: string,
-                         *     methods: string[],
-                         * } $args
-                         */
-                        $args = $attr->getArguments();
-
-                        if (isset($discovered[$args['name']])) {
-                            throw new \RuntimeException(
-                                'A route with this name already exists: '
-                                . $args['name']
-                            );
-                        }
-
-                        $discovered[$args['name']] = new RouteInfo(
-                            name: $args['name'],
-                            path: $args['path'],
-                            httpMethods: $args['methods'],
-                            class: $classInfo->getName(),
-                            method: $method->getName(),
-                        );
-                    }
-                }
-            }
-        }
+        $discovered = $this->discoverRoutes();
 
         foreach ($discovered as $routeInfo) {
             $router = $this->registerOne($router, $routeInfo);
@@ -120,9 +81,64 @@ class RoutesMapper
                 new DirectoriesSourceLocator([$dir['path']], $astLocator)
             );
             $classes = $reflector->reflectAllClasses();
-            $controllers = array_merge($controllers, $classes);
+            array_push($controllers, ...$classes);
+        }
+        return $controllers;
+    }
+
+    private function discoverRoutes(bool $useAPCU): array
+    {
+        // Use this class and method to build the key for saved data
+        $cacheKey = __CLASS__ . '::' . __FUNCTION__;
+        // Check if we find anything and that fetch didn't fail
+        $discovered = $this->useAPCU ? apcu_fetch($cacheKey, $success) : [];
+        if ($discovered && $success) {
+            return $discovered;
         }
 
-        return $controllers;
+        $controllers = $this->findControllers();
+
+        $discovered = [];
+        foreach ($controllers as $classInfo) {
+            $methods = $classInfo->getImmediateMethods(
+                filter: \ReflectionMethod::IS_PUBLIC
+            );
+            foreach ($methods as $method) {
+                if ($method->getName() === '__construct') {
+                    continue;
+                }
+
+                if ($attrs = $method->getAttributesByName(Route::class)) {
+                    foreach ($attrs as $attr) {
+                        /**
+                         * @var array{
+                         *     name: string,
+                         *     path: string,
+                         *     methods: string[],
+                         * } $args
+                         */
+                        $args = $attr->getArguments();
+
+                        if (isset($discovered[$args['name']])) {
+                            throw new \RuntimeException(
+                                'A route with this name already exists: '
+                                . $args['name']
+                            );
+                        }
+
+                        $discovered[$args['name']] = new RouteInfo(
+                            name: $args['name'],
+                            path: $args['path'],
+                            httpMethods: $args['methods'],
+                            class: $classInfo->getName(),
+                            method: $method->getName(),
+                        );
+                    }
+                }
+            }
+        }
+
+        apcu_store($cacheKey, $discovered);
+        return $discovered;
     }
 }
