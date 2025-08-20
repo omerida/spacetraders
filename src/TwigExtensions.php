5211b89efda5fea491ea413e37ace97b9a23fb3f
Oscar Merida <oscar@oscarm.org>
2025-08-19 23:57:18 -0400

    bug fixes

--- src/TwigExtensions.php
+++ src/TwigExtensions.php
@@ -15,46 +15,45 @@ class TwigExtensions
         string $path,
         array $params,
     ): string {
-        return $path . http_build_query($params);
+        return $path . '?' . http_build_query($params);
     }
 
     #[AsTwigFunction('viewMarketplacePath')]
     public static function viewMarketplacePath(string $id): string
     {
-        return self::pathWithParams("/systems/market?", ['id' => $id]);
+        return self::pathWithParams("/systems/market", ['id' => $id]);
     }
 
     #[AsTwigFunction('viewShipCargoPath')]
     public static function viewShipCargoPath(string $id): string
     {
-        return self::pathWithParams("/ship/cargo?ship=", ['id' => $id]);
+        return self::pathWithParams("/ship/cargo", ['ship' => $id]);
     }
 
     #[AsTwigFunction('viewShipPath')]
     public static function viewShipPath(string $id): string
     {
-        return self::pathWithParams("/ship/info?ship=", ['id' => $id]);
+        return self::pathWithParams("/ship/info", ['ship' => $id]);
     }
 
     #[AsTwigFunction('viewShipyardPath')]
     public static function viewShipyardPath(string $id): string
     {
-        return self::pathWithParams("/systems/shipyard?id=", ['id' => $id]);
+        return self::pathWithParams("/systems/shipyard", ['id' => $id]);
     }
 
     #[AsTwigFunction('viewWaypointPath')]
     public static function viewWaypointPath(string $wp): string
     {
-        return self::pathWithParams("/systems/waypoint?id=", ['wp' => $wp]);
+        return self::pathWithParams("/systems/waypoint", ['id' => $wp]);
     }
 
     #[AsTwigFilter('machine2readable')]
     public static function machine2readable(string $input): string
     {
-        // 1. Replace underscores with spaces
         $transformed = str_replace('_', ' ', $input);
 
-        // 2. Uppercase the first letter of each word
+        // Uppercase the first letter of each word
         // ucwords works best if the string is first converted to lowercase
         // to ensure consistent capitalization (e.g., "ALUM_CORE" -> "Alum Core" not "ALUM Core")
         return ucwords(strtolower($transformed));
