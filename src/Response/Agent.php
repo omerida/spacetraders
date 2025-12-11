3933270eb32c0da0c9aa5b570e3bcc12d9e8c931
Oscar Merida <oscar@oscarm.org>
2025-12-11 01:16:18 -0500

    * Move Valinor mapper to trait

--- src/Response/Agent.php
+++ src/Response/Agent.php
@@ -2,16 +2,19 @@
 
 namespace Phparch\SpaceTraders\Response;
 
-use Phparch\SpaceTraders\Value\Waypoint\Symbol;
+use Phparch\SpaceTraders\Trait\MapFromArray;
+use Phparch\SpaceTraders\Value\Waypoint;
 
-class Agent extends Base
+class Agent
 {
+    use MapFromArray;
+
     public function __construct(
         /** @var non-empty-string */
         public readonly string $accountId,
         /** @var non-empty-string */
         public readonly string $symbol,
-        public readonly Symbol $headquarters,
+        public readonly Waypoint\Symbol $headquarters,
         public readonly int $credits,
         /** @var non-empty-string */
         public readonly string $startingFaction,
