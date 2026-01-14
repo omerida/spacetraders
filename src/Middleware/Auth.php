684c6c4a962c566416663c4b9ff7ae1b889dc161
Oscar Merida <oscar@oscarm.org>
2026-01-13 23:30:35 -0500

    [Feature/76] Add phparkitect (#81)
    * Fix #76. Adds phparkitect package and rules for our namespaces.
    
    * Fix #76. Adds phparkitect package and rules for our namespaces.

--- src/Middleware/Auth.php
+++ src/Middleware/Auth.php
@@ -3,7 +3,6 @@
 namespace Phparch\SpaceTraders\Middleware;
 
 use Lcobucci\JWT\Token\InvalidTokenStructure;
-use Phparch\SpaceTraders\APIException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
