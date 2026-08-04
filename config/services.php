75998515968f3bb17940b49fa1acba2a861fdbc9
Oscar Merida <oscar@oscarm.org>
2026-08-04 00:58:57 -0400

    Use Doctrine Entity Manager for persisting to database.

--- config/services.php
+++ config/services.php
@@ -1,6 +1,8 @@
 <?php
 
 use Doctrine\DBAL;
+use Doctrine\ORM;
+use Doctrine\ORM\EntityManager;
 use GuzzleHttp\Psr7;
 use Kevinrob\GuzzleCache\CacheMiddleware;
 use Kevinrob\GuzzleCache\Storage\Psr6CacheStorage;
@@ -118,6 +120,16 @@ return [
         // register events based on attributes on methods in ListenerService
         $provider->addSubscriber(SpaceTraders\Event\ListenerService::class);
         return $provider;
+    },
+    ORM\EntityManager::class => static function(): EntityManager {
+        $config = Doctrine\ORM\ORMSetup::createAttributeMetadataConfig(
+            paths: [__DIR__ .'/../src/Entity'],
+            isDevMode: true,
+        );
+        $config->enableNativeLazyObjects(true);
+
+        $connection = ServiceContainer::get(DBAL\Connection::class);
+        return new EntityManager($connection, $config);
     },
     Routes\Scanner::class => static function () {
         return new Routes\Scanner(
